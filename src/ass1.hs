11ea856bf924a3420305cf3576e278e00ceb6d4a
jkaloger <OldHunterGehrman@gmail.com>
2017-08-12 15:02:48 +1000

    unroll and interleave

--- src/ass1.hs
+++ src/ass1.hs
@@ -8,6 +8,11 @@ subst n1 n2 lst = [if (x == n1) then n2 else x | x <- lst]
 -- interleave takes two lists and returns the interleaving of the two arguments
 interleave :: [t] -> [t] -> [t]
 interleave [] [] = []
+interleave  x [] = x
+interleave []  y = y
+interleave (x:xs) (y:ys) = x : y : interleave xs ys
+
 -- output consists of the input list repeated as many times as necessary to have the specified length
 unroll :: Int -> [a] -> [a]
-unroll 0 [] = []
+unroll 0 _ = []
+unroll n (x:xs) = x : unroll (n-1) (xs ++ [x])
